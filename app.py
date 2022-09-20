d9fbf4f4519fb3aaeb5c5670cce42c9c8c1d9179
NadineHomeyer <nhomeyer@gmx.de>
2022-09-20 17:37:47 +0200

    Status 200922

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ if __name__ == '__main__':
 
     # Add filters/input widgets with tooltips
     st.sidebar.markdown("Filters")
-    selected_group = st.sidebar.radio("Oganism group: ", options=('bony fish', 'sea squirts', 'whales', 'other'), index=0)
+    selected_group = st.sidebar.radio("Oganism group: ", options=('Bony fish', 'Sea squirts', 'Whales', 'Cnidaria', \
+        'Echinodermata', 'Bivalvia', 'Demospongiae', 'Gastropods', 'Polychaetes', 'Gymnolaemata', 'Hexapods', \
+        'Malacostraca', 'Copepods', 'Branchiopoda', 'Other'), index=0)
     
     st.sidebar.markdown("#")
     with open("./annotated_species.txt") as out_file:
@@ -59,28 +61,64 @@ if __name__ == '__main__':
             +"onto the 'Annotate Organism' dropdown below the image, tick the checkbox beside the name of the "
             +"organism, fill in the requested information, and click onto the 'Submit' button. If the page refreshes"
             +"without a warning message the annotation was sucessful and you can annotate of the next organism.")
-    # Read information from file with meta-information
-    df = pd.read_csv("./Chordata_without_avis_v2.txt", sep="\t")
+    st.write("The group of marine Chordata (fish) is displayed per default. Other groups can be displayed by secting "
+            "the radio button of the a group of the sidebar.")
+    st.markdown("<b>Attention</b>: Please make sure you download the annotation file (by clicking the 'Download annotation file' "
+                "button of the side bar) before you leave the page.", unsafe_allow_html=True)
+    # Read information from individual files with meta-information
+    file_names = ["Chordata_without_avis", "Arthropoda", "Bryozoa_and_Annelida", "Cnidaria_and_Echinodermata", "Mollusca_and_Porifera"]
+    dfs = []
+    for file_name in file_names:
+        dfs.append(pd.read_csv("./"+file_name+"_v2.txt", sep="\t"))
+    # Concatenate dataframes into one dataframe
+    df_all_data = pd.concat(dfs, ignore_index=True)
+
     # Filtering
-    assoc_dict = {"bony fish" : "Actinopteri", "sea squirts": "Ascidiacea", "whales": "Mammalia", "other": "other"}
-    groups = ["Actinopteri", "Ascidiacea", "Mammalia"]
+    assoc_dict = {"Bony fish" : "Actinopteri", \
+                "Sea squirts": "Ascidiacea", \
+                "Whales": "Mammalia", \
+                "Cnidaria": "Cnidaria", \
+                "Echinodermata": "Echinodermata", \
+                "Bivalvia": "Bivalvia", \
+                "Demospongiae": "Demospongiae", \
+                "Gastropods": "Gastropoda", \
+                "Polychaetes": "Polychaeta", \
+                "Gymnolaemata": "Gymnolaemata", \
+                "Hexapods": "Hexapoda", \
+                "Malacostraca": "Malacostraca", \
+                "Copepods": "Copepoda", \
+                "Branchiopoda": "Branchiopoda", \
+                "Other": "other"}
+    groups = ["Actinopteri", "Ascidiacea", "Mammalia", "Cnidaria", "Echinodermata", "Bivalvia", \
+            "Demospongiae", "Gastropoda", "Polychaeta", "Gymnolaemata", "Hexapoda", "Malacostraca", \
+            "Copepoda", "Branchiopoda", "other"]
     if assoc_dict[selected_group] != "other":
-        df = df.loc[df["Group"]==assoc_dict[selected_group], :]
+        df_all_data = df_all_data.loc[df_all_data["Group"]==assoc_dict[selected_group], :]
     else:
-        df = df.loc[~df["Group"].isin(groups), :]
+        df_all_data = df_all_data.loc[~df_all_data["Group"].isin(groups), :]
 
     # Read NCBI information
-    df_ncbi = pd.read_csv("./Chordata_without_Avis_NCBItaxreport.txt", sep="|", engine="python")
-    df_ncbi = df_ncbi.drop("code\t" ,axis=1)
-    df_ncbi = df_ncbi.drop("\tprimary taxid\t" ,axis=1)
-    df_ncbi = df_ncbi.rename(columns={"\ttaxid\t": "taxid", "\ttaxname": "taxname"})
-    df_ncbi["taxname"] = df_ncbi["taxname"].replace(to_replace ='\t', value = '', regex=True)
+    dfs_ncbi = []
+    for file_name in file_names:
+        df_ncbi = pd.read_csv("./"+file_name+"_NCBItaxreport.txt", sep="|", engine="python")
+        df_ncbi = df_ncbi.drop("code\t" ,axis=1)
+        if file_name == "Chordata_without_avis":
+            df_ncbi = df_ncbi.drop("\tprimary taxid\t" ,axis=1)
+            df_ncbi = df_ncbi.rename(columns={"\ttaxid\t": "taxid", "\ttaxname": "taxname"})
+            df_ncbi["taxname"] = df_ncbi["taxname"].replace(to_replace ='\t', value = '', regex=True)
+        else:
+            df_ncbi = df_ncbi.drop("\tpreferred name\t" ,axis=1)
+            df_ncbi = df_ncbi.rename(columns={"\ttaxid": "taxid", "\tname\t": "taxname"})
+            df_ncbi["taxname"] = df_ncbi["taxname"].replace(to_replace ='\t', value = '', regex=True)
+            df_ncbi["taxid"] = df_ncbi["taxid"].replace(to_replace ='\t', value = '', regex=True)
+        dfs_ncbi.append(df_ncbi)
+    df_ncbi_all = pd.concat(dfs_ncbi, ignore_index=True)
 
     # Define columns
     pad1, col, pad2 = st.columns((4,6,4))
 
     with col:
-        for i, row in df.iterrows():
+        for i, row in df_all_data.iterrows():
             # Include Species name
             st.subheader(row["Species"])
             # Include Image
@@ -89,7 +127,7 @@ if __name__ == '__main__':
             worms_link='WoRMS: [Link]({link})'.format(link="https://www.marinespecies.org/aphia.php?p=taxdetails&id="+str(row["AphisID"]))
             st.markdown(worms_link, unsafe_allow_html=True)
             # Link to NCBI
-            df_tmp = df_ncbi.loc[df_ncbi["taxname"]==row["Species"], :]
+            df_tmp = df_ncbi_all.loc[df_ncbi_all["taxname"]==row["Species"], :]
             if not(df_tmp.empty):
                 ncbi_link='NCBI Taxonomy: [Link]({link})'.format(link="https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id="+str(list(df_tmp["taxid"])))
                 st.markdown(ncbi_link, unsafe_allow_html=True)
